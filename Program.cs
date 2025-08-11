284200909f1d7d7eb4c3372460c3b06a2836dbe9
Abdul <yasdanis@gmail.com>
2025-08-11 12:52:08 +0530

    GenericList Person added

--- Program.cs
+++ Program.cs
@@ -4,6 +4,39 @@ using MyFirstConsoleIBMproject;
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Welcome To C#.Net Programing Fundamentals.");
 
+GenericCollections GenericCollections = new GenericCollections();
+
+//adding persons to the generic list
+Console.WriteLine("How many persons you want to add to the generic list:");
+int personCount = Convert.ToInt32(Console.ReadLine());
+
+for(int i = 0;i < personCount; i++)
+{
+   //GenericCollections.AddPerson(new Person());
+    Console.WriteLine($"Please enter details for person {i + 1}:");
+    Console.WriteLine("Please enter your ID:");
+    int id = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Please enter your Name:");
+    string? name = Console.ReadLine();
+    Console.WriteLine("Please enter your City:");
+    string? city = Console.ReadLine();
+    Console.WriteLine("Please enter your Age:");
+    int age = Convert.ToInt32(Console.ReadLine());
+   
+
+    Person person = new Person(id, name, city, age);
+    GenericCollections.AddPerson(person);
+}
+// Displaying all the persons in the generic list
+
+Console.WriteLine("\nPerson details from  the generic list...");
+
+// Displaying the persons in the generic list
+Console.WriteLine("\nPersons in the list:");
+foreach (var person in GenericCollections.persons)
+{
+    person.DisplayPersonDetails();
+}
 
 
 //GenericStringNames();
